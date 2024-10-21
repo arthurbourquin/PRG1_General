a2bd98c9f57c5a872ecf7feb11ab225251ccb150
arthur <arthur@tohm.ch>
2024-10-21 12:25:49 +0200

    révisions, pointeurs, ajout du fichier Liste_Des_Commandes.md

--- 05-Flux/redirection-cin.cpp
+++ 05-Flux/redirection-cin.cpp
@@ -1,106 +1,47 @@
-# Redirection de cin, copier un fichier dans un autre
-
-Modifiez le programme en C++ de l'exercice 01-02 pour ajouter les fonctionnalités suivantes :
-
-- Demander à l'utilisateur de saisir le nom d'un fichier source.
-- Au lieu de saisir le texte, utiliser cin pour lire depuis le fichier source et ajouter (append) le texte dans le fichier de sortie. Si le fichier de sortie existe déjà, il ne doit pas être écrasé.
-
-
-Ps. Le code utilisé pour l'écriture ne doit pas changer, comparé à l'exercice 01-02.   
-
-<details>
-<summary>Solution</summary>
-
-~~~cpp
 #include <iostream>
 #include <fstream>
 #include <string>
+using namespace std;
 
 bool lire_fichier(const std::string& nom_fichier){
-    std::ifstream fichier_entree(nom_fichier);
-
-    // Vérifiez si l'ouverture du fichier a réussi
-    if (!fichier_entree) {
-        return false;
+    ifstream SkiaDdans;
+    SkiaDdans.open(nom_fichier);
+    while(SkiaDdans) {
+        string lalaligne;
+        getline(SkiaDdans, lalaligne);
+        cout << lalaligne << '\n';
     }
-
-    while (fichier_entree) {
-        std::string strInput;
-        std::getline(fichier_entree, strInput); // lire une ligne
-        std::cout << strInput << "\n";
-    }
-
-    fichier_entree.close();
-
-    return true;
+    SkiaDdans.close();
+    return 0;
 }
 
 bool ecrire_fichier(const std::string& nom_fichier) {
-
-    // Ouvrez le fichier en mode append
-    std::ofstream fichier_sortie(nom_fichier, std::ios::app);
-
-    // Vérifiez si l'ouverture du fichier a réussi
-    if (!fichier_sortie) {
-        std::cerr << "Erreur : Impossible d'ouvrir le fichier. \n";
-        return false;
+    ofstream SkonMeDdans;
+    SkonMeDdans.open(nom_fichier);
+    if(!SkonMeDdans) {
+        cerr << "Ah ça marche pas !" << endl;
+        return 1;
+    }
+    cout << "Ecrire 'STOP' pour arrêter ce truc." << endl;
+    string LUtilisateurIlAEcritCa;
+    cin >> LUtilisateurIlAEcritCa;
+    while(true) {
+        SkonMeDdans << LUtilisateurIlAEcritCa << '\n';
+        if(LUtilisateurIlAEcritCa == "STOP") break;
     }
 
-    std::string texte;
-    const std::string terminer = "#exit#";
-
-    // Demandez à l'utilisateur de saisir du texte
-    std::cout << "Entrez le texte à enregistrer dans le fichier (Ctrl+D ou #exit# pour terminer la saisie) :\n";
-    while (std::getline(std::cin, texte)) {
-        if (texte == terminer) break;
-        // Écrivez le texte dans le fichier
-        fichier_sortie << texte << std::endl;
-    }
-
-    // Fermez le fichier
-    fichier_sortie.close();
-
-    std::cout << "Le texte a été enregistré avec succès dans le fichier." << std::endl;
-
-    return true;
+    return 0;
 }
 
 int main() {
     std::string nom_fichier;
 
-    // Demandez à l'utilisateur le nom du fichier où enregistrer le texte
     std::cout << "Entrez le nom du fichier où enregistrer le texte : ";
     std::getline(std::cin, nom_fichier);
 
     lire_fichier(nom_fichier);
 
-    std::string nom_fichier_src;
-    // Demandez à l'utilisateur le nom du fichier source
-    std::cout << "Entrez le nom du fichier source : ";
-    std::getline(std::cin, nom_fichier_src);
-
-    std::ifstream fichier_entree(nom_fichier_src);
-
-    // Vérifiez si l'ouverture du fichier a réussi
-    if (!fichier_entree) {
-        std::cerr << "Erreur : Impossible d'ouvrir le fichier. \n";
-        return EXIT_FAILURE;
-    }
-
-    auto cin_buf = std::cin.rdbuf();
-    std::cin.rdbuf(fichier_entree.rdbuf());
-
     ecrire_fichier(nom_fichier);
-
-    std::cin.rdbuf(cin_buf);
-    fichier_entree.close();
-
 
     return EXIT_SUCCESS;
 }
-
-~~~
-
-
-
-</details>
---
+++ Liste_Des_Commandes.md
@@ -0,0 +1,32 @@
+~~~cpp
+/*
+comment
+*/
+#include <iostream> // comment
+#include <cstdlib>
+using namespace std;
+
+int main() {}
+
+cout << "Hello world" << endl;
+cin >> a;
+
+bool a = true;
+(int a; char a;) (float a; double a; long a;) short / long / long long, signed / unsigned
+
+std::string a = "Hello";
+std::array<double, 4> g = {1.0, 1.5, 3.1415926535, 2.0}; std::array a { 1, 2, 3};
+std::vector<int> h = {1, 2, 3, 4};
+std::tuple<bool, int, float, double, char, string> g = {true, 1, 1.5f, 3.1415926535, 'x', "ouais"};
+
+int i = 10;     // Déclaration d'un entier
+int* ptr = &i;  // ptr contient l'adresse de i (pointeur)
+int& ref = i;   // ref est une référence à i
+*ptr = 20;      // Modifie i via le pointeur
+ref = 30;       // Modifie également i via la référence
+
+++i i++
+'a' + 1 = 'b'
+string("Hello") + " world"
+
+~~~
